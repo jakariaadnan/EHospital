718a65c19e3bf67f5fe8e4d745d4dba13c327249
jakariaadnan <j.adnanmiya@gmail.com>
2024-02-15 01:58:04 +0600

    gg

---
+++ HospitalManagementSystem/Areas/Public/Views/Home/AppoinmentVM.cs
@@ -0,0 +1,12 @@
+﻿namespace HospitalManagementSystem.Areas.Public.Views.Home
+{
+    public class AppoinmentVM
+    {
+        public string? pName { get; set; }
+        public string? pAdd { get; set; }
+        public string? pPhone { get; set; }
+        public int? deptId { get; set; }
+        public int? docId { get; set; }
+        public DateTime date { get; set; }
+    }
+}
